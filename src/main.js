65d79a4b58f6eff61edfe801b9e6ad70d716abc1
Cédric Finance <cedric.finance@finandric.com>
2016-05-23 22:26:28 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,108 @@
+var fs = require('fs')
+
+debug_enabled = true
+
+function debug(message) {
+  if (debug_enabled) {
+    console.error(message);
+  }
+}
+
+var registers = new Array(8).fill(0)
+var memory = []
+var data = fs.readFileSync('challenge.bin')
+for(var i=0; i < data.byteLength/2; i++) {
+  memory.push(data.readUInt16LE(2*i))
+}
+
+var pc = 0
+
+function decodeValue(v) {
+  if (v < 32767) {
+    return v
+  }
+  return registers[v - 32768]
+}
+
+function next() {
+  var value = memory[pc]
+  pc++
+  return value;
+}
+
+while (true) {
+
+  var oldPC = pc
+  var opcode = next();
+
+  switch (opcode) {
+    case 0:
+      process.exit(1)
+      break;
+    case 1:
+      var register = next();
+      var value = next()
+      debug(`PC: ${oldPC} set ${register} ${value}`)
+      registers[register-32768] = value
+      break
+    case 4:
+      var dest = next();
+      var rawA = next();
+      var rawB = next();
+      debug(`PC: ${oldPC} EQ ${dest} ${rawA} ${rawB}`);
+      if (decodeValue(a) === decodeValue(b)) {
+        registers[dest - 32768] = 1
+      } else {
+        registers[dest - 32768] = 0
+      }
+      break;
+    case 6:
+      var address = next();
+      //debug(`PC: ${oldPC} JMP ${address}`);
+      pc = address;
+      break;
+    case 7:
+      var value = next()
+      var address = next()
+      debug(`PC: ${oldPC} JT ${value} ${address}`);
+      if (value > 32767) {
+        value = registers[value - 32768]
+      }
+      if (value !== 0) {
+        pc = address
+      }
+      break;
+    case 8:
+      var value = next()
+      var address = next()
+      debug(`PC: ${oldPC} JF ${value} ${address}`);
+      if (value > 32767) {
+        value = registers[value - 32768]
+      }
+      if (value === 0) {
+        pc = address
+      }
+      break;
+    case 9:
+      var dest = next()
+      var a = next()
+      var b = next()
+      debug(`PC: ${oldPC} ADD ${dest} ${a} ${b}`);
+      registers[dest - 32768] = a + b
+      break;
+    case 19:
+      var char = next()
+      var c = String.fromCharCode(char)
+      //debug(`PC: ${oldPC} putc ${c}`);
+      process.stdout.write(c);
+      break;
+    break;
+    case 21:
+      //debug(`PC: ${oldPC} NOOP`);
+      break;
+    default:
+      throw new Error(`Unknown opcode: ${opcode}`)
+
+  }
+
+}
